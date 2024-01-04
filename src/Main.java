f9c0b93fabc317f8cfada375acac50d0ea90bb1a
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-01-04 09:42:38 -0500

    dsrh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+
+        int [] vals = {1, 2, 3, 4, 6};
+        shiftRight(vals);
+        for (int i = 0; i < vals.length; i++) {
+            System.out.println(vals[i] + " ");
+        }
+    }
+
+    public static int [] insert(int [] arr, int element, int index) {
+        int [] nums = arr;
+        for (int i = nums.length - 1; i > index; i--) {
+            nums[i]= nums[i-1];
+        }
+        nums[index] = element;
+        return nums;
+    }
+
+    public static int [] shiftRight(int [] arr){
+        int[] nums = arr;
+        int temp = nums[nums.length - 1];
+        for (int i = nums.length - 1; i > 0; i--) {
+            nums[i] = nums [i-1];
+        }
+        nums[0] = temp;
+        return nums;
     }
 }
